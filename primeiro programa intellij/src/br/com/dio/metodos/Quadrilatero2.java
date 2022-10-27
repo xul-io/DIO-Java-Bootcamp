21bed0906de9bddec432b8d8351203773388201e
Julio <juliothomaczyck@gmail.com>
2022-10-27 19:37:11 -0300

    Adicionando Quadrilatero2

---
+++ primeiro programa intellij/src/br/com/dio/metodos/Quadrilatero2.java
@@ -0,0 +1,44 @@
+package br.com.dio.metodos;
+
+public class Quadrilatero2 {
+    public static void main(String[] args) {
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = area(3);
+        System.out.println("Área do quadrado:" + areaQuadrado);
+
+        double areaRetangulo = area(5,5);
+        System.out.println("Área do retângulo:" + areaRetangulo);
+
+        double areaTrapezio = area(7,8,9);
+        System.out.println("Área do trapézio:" + areaTrapezio);
+    }
+
+    public static double area(double lado) {
+
+        return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2) {
+
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura) {
+
+        return ((baseMaior+baseMenor)*altura) / 2;
+    }
+
+    public static void xpto() {
+
+        System.out.println("Antes");
+        return;
+    }
+
+    public static double abc() {
+        return 1.6;
+    }
+
+
+
+}
