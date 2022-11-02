d8ce789d5f0eb2e9c0faf3d2ebb45b4be2523133
Julio <juliothomaczyck@gmail.com>
2022-11-02 20:39:06 -0300

    Adicionando novos comentários pt1

--- primeiro programa intellij/src/br/com/dio/tratamentoDeExcecoes/CepInvalidoException.java
+++ primeiro programa intellij/src/br/com/dio/tratamentoDeExcecoes/CepInvalidoException.java
@@ -1,5 +1,5 @@
 package br.com.dio.tratamentoDeExcecoes;
-
+// extends traz novas características para um classe
 public class CepInvalidoException extends Exception{
     public static void main(String[] args) {
 
